00cb5c51ea3b002227594730b6dbf2f5bd3e8c01
DESKTOP-R36JGBR\HAI <nn.minhhai@gmail.com>
2021-08-01 12:16:55 +0700

    commit migration an repository

---
+++ Shop.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+
+namespace Shop.Data.Repositories
+{
+    public interface IOrderRepository  : IRepository<Order>
+    {
+    }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
