ddd09cceff3ca7df588cbd7a4908563ff8a62fbe
DESKTOP-R36JGBR\HAI <nn.minhhai@gmail.com>
2021-08-22 05:52:24 +0700

    paging for list

--- Shop.Data/Migrations/Configuration.cs
+++ Shop.Data/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace Shop.Data.Migrations
 {
+    using Common;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using Model.Models;
+    using System;
     using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,35 +19,36 @@ namespace Shop.Data.Migrations
         protected override void Seed(Shop.Data.ShopDbContext context)
         {
             CreateProductCategorySample(context);
+            CreateUser(context);
             //  This method will be called after migrating to the latest version.
         }
 
-        private void CreateUser(ShopDbContext context)
+        private void CreateUser(Shop.Data.ShopDbContext context)
         {
-            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShopDbContext()));
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShopDbContext()));
 
-            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShopDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShopDbContext()));
 
-            //var user = new ApplicationUser()
-            //{
-            //    UserName = "nguyenhai",
-            //    Email = "nguyenhai@gmail.com",
-            //    EmailConfirmed = true,
-            //    BirthDay = DateTime.Now,
-            //    FullName = "Nguyen Ngoc Minh Hai"
-            //};
+            var user = new ApplicationUser()
+            {
+                UserName = "nguyenhai",
+                Email = "nguyenhai@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Nguyen Ngoc Minh Hai"
+            };
 
-            //manager.Create(user, "123456");
+            manager.Create(user, "123456@");
 
-            //if (!roleManager.Roles.Any())
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "User" });
-            //}
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
 
-            //var adminUser = manager.FindByEmail("nguyenhai@gmail.com");
+            var adminUser = manager.FindByEmail("nguyenhai@gmail.com");
 
-            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
         }
 
         private void CreateProductCategorySample(Shop.Data.ShopDbContext context)
@@ -61,5 +66,7 @@ namespace Shop.Data.Migrations
                 context.SaveChanges();
             }
         }
+
+
     }
 }
